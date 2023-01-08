825defa766bb05bc8422d3f6c6b71878f1e2dbe6
yifan <yshenaz@connect.ust.hk>
2023-01-08 08:09:27 +0800

    Since we have binary install of custom messages we dont have to source the workspace anymore

--- launch_attention.bash
+++ launch_attention.bash
@@ -1,3 +1,3 @@
 source ./Grace_Connector/IP_Setup_Local.bash #ip config for ros connection
-source ./Grace_Attention/msg_catkin_ws/devel/setup.bash #msg package referencing
+#source ./Grace_Attention/msg_catkin_ws/devel/setup.bash #msg package referencing
 python3 -m Grace_Attention.grace_attention
